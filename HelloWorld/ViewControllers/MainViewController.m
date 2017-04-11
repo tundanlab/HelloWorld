2b444da79583348fd1496a8b00915a136389a3a1
TuND <tu.nd@anlab.info>
2017-04-11 09:01:49 +0700

    - add update button

--- HelloWorld/ViewControllers/MainViewController.m
+++ HelloWorld/ViewControllers/MainViewController.m
@@ -11,6 +11,7 @@
 @interface MainViewController ()
 
 @property (nonatomic, strong) UITextField * mainTextField;
+@property (nonatomic, strong) UIButton * updateButton;
 
 @end
 
@@ -25,7 +26,8 @@
 - (void) viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
-    [self createTextField];
+    [self createMainTextField];
+    [self createUpdateButton];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -35,11 +37,20 @@
 
 #pragma mark - CreateSomeView
 
-- (void) createTextField {
+- (void) createMainTextField {
     float x = 40;
     self.mainTextField = [[UITextField alloc] initWithFrame:CGRectMake(x, 100, [UIScreen mainScreen].bounds.size.width - 2 * x, 30)];
     self.mainTextField.borderStyle = UITextBorderStyleRoundedRect;
     [self.view addSubview:self.mainTextField];
+}
+
+- (void) createUpdateButton {
+    self.updateButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.updateButton.frame = CGRectMake(0, CGRectGetMaxY(self.mainTextField.frame) + 40.0, 100, self.mainTextField.frame.size.height);
+    self.updateButton.center = CGPointMake(self.view.center.x, self.updateButton.center.y);
+    [self.updateButton setTitle:@"Update" forState:UIControlStateNormal];
+    [self.updateButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [self.view addSubview:self.updateButton];
 }
 
 @end
